c8b0212d15fb3db86bd4fc888bb4fc16e573e213
Hatchin <sangyushen@gmail.com>
2019-12-16 23:14:45 -0800

    paginate

--- _i18n/zh/_pages/category.md
+++
@@ -1,9 +0,0 @@
----
-title: "Posts by Category"
-layout: categories
-language: zh
-handle: /categories/
-author_profile: false
----
-
-
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,16 +1,16 @@
 var store = [{
-        "title": "Mann Whitney U Test - Overview",
+        "title": {"en":"Mann Whitney U Test - Overview"},
         "excerpt":"In this series, I am going to take a journey on Mann-Whitney U test and my Python solution for testing on small sample size. There is going to be an extension for original Mann-Whitney U test in Scipy. Because the current scipy version only support test for large sample size(n&gt;20),...","categories": ["Stat"],
         "tags": ["Theory"],
-        "url": "http://localhost:4000/stat/2019/05/07/Mann-Whitney-U.html",
+        "url": "http://localhost:4000/mannwhitneyu/",
         "teaser":"http://localhost:4000/assets/images/teaser.JPG"},{
-        "title": "Mann Whitney U Test - with Python Solution for Small Sample",
+        "title": {"en":"Mann Whitney U Test - with Python Solution for Small Sample"},
         "excerpt":"In this part, I am going to show a demo on the Mann-Whitney U Test Python script which is able to perform test on both small and large sample size. Mann-Whitney U test is very useful in small data sample cases, such as clinical dataset (many clinical trials only have...","categories": ["Stat"],
-        "tags": ["Tool","Web App"],
-        "url": "http://localhost:4000/stat/2019/05/08/Mann-Whitney-U-Tool.html",
+        "tags": ["Tool","WebApp"],
+        "url": "http://localhost:4000/mannwhitneyutool/",
         "teaser":"http://localhost:4000/assets/images/teaser.JPG"},{
-        "title": "FlowSOM: to analyze flow or mass cytometry data using a self‐organizing map",
-        "excerpt":"Backgroud FlowSOM(Van Gassen et al., 2015)[1] is one of the available algorithms for flow cytometry and high-dimensional data analysis. Flow cytometry is a technique which could detect and measure characteristics of many thousand cells or particles per second. The high‐dimensional flow cytometry data have made it possible to detect protein...","categories": ["Neural Network","Clustering"],
+        "title": {"en":"FlowSOM: to analyze flow or mass cytometry data using a self‐organizing map"},
+        "excerpt":"Backgroud FlowSOM(Van Gassen et al., 2015)[1] is one of the available algorithms for flow cytometry and high-dimensional data analysis. Flow cytometry is a technique which could detect and measure characteristics of many thousand cells or particles per second. The high‐dimensional flow cytometry data have made it possible to detect protein...","categories": ["Neural_Network","Clustering"],
         "tags": ["Theory"],
-        "url": "http://localhost:4000/neural%20network/clustering/2019/08/30/FlowSOM.html",
+        "url": "http://localhost:4000/flowsom/",
         "teaser":"http://localhost:4000/assets/images/teaser.JPG"}]
